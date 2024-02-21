f5fc99f2a8bd808969ff328b47f95388b02cd1bc
J. Scott Branson <the@rabbitkick.club>
2024-02-21 17:25:26 -0500

    Optimize tallies example and update requirements.

--- tallies.py
+++ tallies.py
@@ -12,19 +12,36 @@ c = Counter()
 v = Counter()
 d = Counter()
 p = Counter()
+
+ips = []
+
 for peer in peers:
     try:
+        ips.append(peer['ip'])
+    except KeyError:
+        pass
+    try:
         v[peer["version"]] +=1
+    except KeyError:
+        pass
+    try:
         c[peer["country"]] +=1
+    except KeyError:
+        pass
+    try:
         d[peer["type"]] +=1
+    except KeyError:
+        pass
+    try:
         p[str(peer["port"])] +=1
     except KeyError:
         pass
 
 open(OUTPUT_FILE, "w").close()
 with open(OUTPUT_FILE, "a") as output:
-    output.write(str(c))
-    output.write("\n" + str(v))
-    output.write("\n" + str(d))
-    output.write("\n" + str(p))
-    output.write("\nTotal Peers: " + str(c.total()))
+    output.write("Server Country:\n" + str(c))
+    output.write("\n\nServer Version:\n" + str(v))
+    output.write("\n\nConnection Direction:\n" + str(d))
+    output.write("\n\nPeer Port:\n" + str(p))
+    output.write("\n\nTotal Peers: " + str(c.total()))
+    output.write("\n\nTotal IPs: " + str(len(ips)))
